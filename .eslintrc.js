fd0c3a0efc7f48ead0ba9e5f7739ce7fa9fc29d6
Schalk Venter <venterschalk@gmail.com>
2020-06-26 08:36:25 +0200

    Structures initial monorepo skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:sonarjs/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'prettier',
+    'jest',
+    'compat',
+    'sonarjs',
+    'optimize-regex',
+  ],
+  env: {
+    'jest/globals': true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'sonarjs/cognitive-complexity': ['warn', 20],
+    'sonarjs/no-duplicate-string': 'warn',
+  },
+};
